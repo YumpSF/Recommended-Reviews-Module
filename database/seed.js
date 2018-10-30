ae55ca9e1a72aa022f1c57e518c5ace7518bf7c7
Danny <ngo.danny11@gmail.com>
2018-10-30 12:42:54 -0700

    API routes to restaurant_id and renders all reviews for that restaurant_id

--- database/seed.js
+++ database/seed.js
@@ -7,8 +7,8 @@ for (let i = 0; i < 100; i++) {
   var obj = {};
   obj.name = faker.company.companyName();
   dataRestaurant.push(obj);
-  const query = `Insert INTO restaurant(name) VALUES(?)`;
-  db.query(query, [obj.name], (err) => {
+  const queryString = `Insert INTO restaurant(name) VALUES(?)`;
+  db.connection.query(queryString, [obj.name], (err) => {
     if (err) {
       console.log(err);
     }
@@ -19,13 +19,13 @@ for (let i = 0; i < 100; i++) {
 var dataUserInfo = [];
 for (let i = 0; i < 100; i++) {
   var obj = {};
-  obj.name = faker.name.findName();
+  obj.user_name = faker.name.findName();
   obj.location = faker.address.city() + ' ' + faker.address.state();
   obj.number_reviews = Math.floor(Math.random() * 50) + 1;
   obj.number_photos = Math.floor(Math.random() * 40) + 1;
   dataUserInfo.push(obj);
-  const query = `Insert INTO user_info(name, location, number_reviews, number_photos) VALUES(?, ?, ?, ?)`;
-  db.query(query, [obj.name, obj.location, obj.number_reviews, obj.number_photos], (err) => {
+  const queryString = `Insert INTO user_info(user_name, location, number_reviews, number_photos) VALUES(?, ?, ?, ?)`;
+  db.connection.query(queryString, [obj.user_name, obj.location, obj.number_reviews, obj.number_photos], (err) => {
     if (err) {
       console.log(err);
     }
@@ -43,8 +43,8 @@ for (let i = 0; i < 1000; i++) {
   obj.picture_food = faker.image.food();
   obj.restaurant_id = Math.floor(Math.random() * 100) + 1;
   dataUserReview.push(obj);
-  const query = `Insert INTO users_reviews(user_id, date, review_comment, score, picture_food, restaurant_id) VALUES(?, ?, ?, ?, ?, ?)`;
-  db.query(query, [obj.user_id, obj.date, obj.review_comment, obj.score, obj.picture_food, obj.restaurant_id], (err) => {
+  const queryString = `Insert INTO users_reviews(user_id, date, review_comment, score, picture_food, restaurant_id) VALUES(?, ?, ?, ?, ?, ?)`;
+  db.connection.query(queryString, [obj.user_id, obj.date, obj.review_comment, obj.score, obj.picture_food, obj.restaurant_id], (err) => {
     if (err) {
       console.log(err);
     }
