f2612980071837b24f3f3361ac2ac23ab9ad86ac
Danny <ngo.danny11@gmail.com>
2018-11-05 21:37:27 -0800

    make changes for proxy connection

--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from '../components/app.js';
 
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('recommended-reviews'));
