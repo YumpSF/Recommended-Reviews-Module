21de967b31d0c97b47073d16f7ba29eec9813922
Danny <ngo.danny11@gmail.com>
2018-11-10 15:45:42 -0800

    Refactor to compress bundle.js to gzip file

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
 const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-    entry: "./client/src/index.js", //tells where webpack to start looking for files
-    output: {
-        path: path.resolve(__dirname, 'public/dist'), //folder where webpack will put bundle
-        filename: 'bundled.js', //what the file be named
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: "babel-loader",
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".js", ".jsx"]
-    },
-    mode: 'development',
+  plugins: [
+    new CompressionPlugin(),
+  ],
+  entry: './client/src/index.js', //tells where webpack to start looking for files
+  output: {
+    path: path.resolve(__dirname, 'public/dist'), //folder where webpack will put bundle
+    filename: 'bundled.js', //what the file be named
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  mode: 'development',
 };
