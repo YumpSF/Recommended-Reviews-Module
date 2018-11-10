98b98025bd8f55044fad0b009c1e96030e2f2c49
Danny <ngo.danny11@gmail.com>
2018-11-10 11:32:36 -0800

    Added CORS headers for server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,12 @@ var bodyParser = require('body-parser');
 var db = require('../database/index.js');
 var path = require ('path');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
